b07a5146fecd1e197773b647b818c545e19e0a5c
tvk134 <46226474+tvk134@users.noreply.github.com>
2019-01-06 11:30:26 -0500

    Update Reverse_Baap_Re.java

--- Reverse_Baap_Re.java
+++ Reverse_Baap_Re.java
@@ -3,7 +3,11 @@ package Stuff;
 public class Reverse_Baap_Re {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			int[] twenty = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+		for(int i = 0;i<twenty.length;i++) 
+		{
+			System.out.println(twenty[i]);
+		}
 
 	}
 
