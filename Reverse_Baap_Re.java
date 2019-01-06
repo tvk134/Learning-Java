4a788f1e475255be8f4fd47e2ee8957ef7ab58db
tvk134 <46226474+tvk134@users.noreply.github.com>
2019-01-06 12:07:16 -0500

    Update Reverse_Baap_Re.java

--- Reverse_Baap_Re.java
+++ Reverse_Baap_Re.java
@@ -4,7 +4,7 @@ public class Reverse_Baap_Re {
 
 	public static void main(String[] args) {
 			int[] twenty = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
-		for(int i = 19;i>=0;i--) 
+		for(int i = twent.length();i>=0;i--) 
 		{
 			System.out.println(twenty[i]);
 		}
