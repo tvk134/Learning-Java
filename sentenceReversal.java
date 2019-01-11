087a6a6540fca77aa10a58739d4f37560d5f7a1b
tvk134 <46226474+tvk134@users.noreply.github.com>
2019-01-11 17:04:58 -0500

    Add files via upload

---
+++ sentenceReversal.java
@@ -0,0 +1,14 @@
+package Stuff;
+
+public class sentenceReversal {
+
+	public static void main(String[] args) {
+		String[] sentence = {"My","Name","is","Tanay","Kale"};
+		for(int i=4;i>=0;i--)
+		{
+			System.out.print(sentence[i]+" ");
+		}
+
+	}
+
+}
